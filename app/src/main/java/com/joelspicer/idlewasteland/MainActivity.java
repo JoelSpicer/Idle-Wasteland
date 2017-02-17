159009ca24c6cca900baefd83f2bd25893ff7575
Joel Spicer <joel.spicer@btinternet.com>
2017-02-17 17:32:42 +0000

    Background added

--- app/src/main/java/com/joelspicer/idlewasteland/MainActivity.java
+++ app/src/main/java/com/joelspicer/idlewasteland/MainActivity.java
@@ -1,13 +1,23 @@
 package com.joelspicer.idlewasteland;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //turn title off
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //set to fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(new GamePanel(this));
     }
 }
